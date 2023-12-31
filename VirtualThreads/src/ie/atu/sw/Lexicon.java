3386ff2027032826b52e3d8dad8f25366efd5954
Nelson Almeida <nelsonconceicao1@gmail.com>
2023-12-31 15:51:29 +0000

    first commit

---
+++ VirtualThreads/src/ie/atu/sw/Lexicon.java
@@ -0,0 +1,69 @@
+package ie.atu.sw;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+/*Lexicon Entry
+public class LexiconEntry {
+	private String word;
+	private int score;
+
+	
+	 * Constructs a LexiconEntry with the specified word and score.
+	 *
+	 * @param word the word in the lexicon.
+	 * 
+	 * @param score the sentiment score associated with the word.
+	 
+	public LexiconEntry(String word, int score) {
+		this.word = word;
+		this.score = score;
+	}
+
+//Getters and setters for word and score 
+}
+
+//Loads and manages a lexicon with words and their associated sentiment scores.
+public class LexiconLoader {
+	private Map<String, Integer> lexiconMap = new ConcurrentHashMap<>();
+
+	/*
+	 * Loads lexicon entries from a file into the lexiconMap.
+	 *
+	 * @param filePath the path to the lexicon file.
+	 
+	public void loadLexiconFromFile(String filePath) {
+		// Implements file reading and populate lexiconMap
+	}
+}
+
+//Analyzes the sentiment of tweets using a lexicon.
+//Tweet Analyzer
+public class TweetAnalyzer {
+	private LexiconLoader lexiconLoader;
+
+	public TweetAnalyzer(LexiconLoader lexiconLoader) {
+		this.lexiconLoader = lexiconLoader;
+	}
+
+	public int analyzeTweet(String tweet) {
+		// Implement sentiment analysis logic
+		// Return sentiment score
+	}
+}
+
+//Main Application
+public class SentimentAnalysisApp {
+	public static void main(String[] args) {
+     LexiconLoader lexiconLoader = new LexiconLoader();
+     lexiconLoader.loadLexiconFromFile("C:\Users\Morpheus\Desktop\New folder\lexicons\lexicons\afinn.txt");
+     
+     TweetAnalyzer tweetAnalyzer = new TweetAnalyzer(lexiconLoader);
+     
+     // Sample tweet
+     String tweet = "tweet example";
+     int sentimentScore = tweetAnalyzer.analyzeTweet(tweet);
+     System.out.println("Sentiment Score: " + sentimentScore);
+     
+ }
+}*/
---
+++ VirtualThreads/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module VirtualThreads {
+}
